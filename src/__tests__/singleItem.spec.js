cef473dd7f8cbb7232e5580fc0ea3a3f3d714eb7
lautaroleonhardt <lautaro.leonhardt01@gmail.com>
2023-07-04 20:31:33 -0300

    test: singleItem.spec.js -> update

--- src/__tests__/singleItem.spec.js
+++ src/__tests__/singleItem.spec.js
@@ -3,35 +3,55 @@ import user from '@testing-library/user-event'
 import SingleItem from '../SingleItem'
 
 describe('SingleItem', () => {
-  let item, mock
+  let item
+  const mockRemoveItem = jest.fn()
+  const mockEditItem = jest.fn()
 
-  beforeEach(() => {
-    item = { id: 1, name: 'test', completed: false }
-    mock = jest.fn()
-    render(<SingleItem item={item} removeItem={mock} editItem={mock} />)
+  describe('when item is not completed', () => {
+    beforeEach(() => {
+      item = { id: 1, name: 'test', completed: false }
+      render(<SingleItem item={item} removeItem={mockRemoveItem} editItem={mockEditItem} />)
+    })
+
+    it('should render the item name', () => {
+      expect(screen.getByText(/test/i)).toBeInTheDocument()
+    })
+
+    it('should render edit button', () => {
+      expect(screen.getByRole('button', /edit/i)).toBeInTheDocument()
+    })
+
+    it('should render remove button', () => {
+      expect(screen.getByRole('button', /remove/i)).toBeInTheDocument()
+    })
+
+    it('should call removeItem when remove button is clicked', async () => {
+      const removeButton = screen.getByRole('button', /remove/i)
+      await user.click(removeButton)
+      expect(mockRemoveItem).toHaveBeenCalledTimes(1)
+    })
+
+    it('should call editItem when edit button is clicked', async () => {
+      const editButton = screen.getByRole('checkbox')
+      await user.click(editButton)
+      expect(mockEditItem).toHaveBeenCalledTimes(1)
+    })
+
+    it('should not render line-through decoration if item is not completed', () => {
+      const item = screen.getByText('test')
+      expect(item).not.toHaveStyle('text-decoration: line-through')
+    })
   })
 
-  it('should render the item name', () => {
-    expect(screen.getByText(/test/i)).toBeInTheDocument()
-  })
+  describe('when item is completed', () => {
+    beforeEach(() => {
+      item = { id: 1, name: 'test', completed: true }
+      render(<SingleItem item={item} removeItem={mockRemoveItem} editItem={mockEditItem} />)
+    })
 
-  it('should render edit button', () => {
-    expect(screen.getByRole('button', /edit/i)).toBeInTheDocument()
-  })
-
-  it('should render remove button', () => {
-    expect(screen.getByRole('button', /remove/i)).toBeInTheDocument()
-  })
-
-  it('should call removeItem when remove button is clicked', async () => {
-    const removeButton = screen.getByRole('button', /remove/i)
-    await user.click(removeButton)
-    expect(mock).toHaveBeenCalledTimes(1)
-  })
-
-  it('should call editItem when edit button is clicked', async () => {
-    const editButton = screen.getByRole('button', /edit/i)
-    await user.click(editButton)
-    expect(mock).toHaveBeenCalledTimes(1)
+    it('should render line-through decoration if item is completed', () => {
+      const item = screen.getByText('test')
+      expect(item).toHaveStyle('text-decoration: line-through')
+    })
   })
 })
