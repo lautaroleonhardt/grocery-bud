f50f35360d151519dd6a5e77d860f8bb4264ccb1
lautaroleonhardt <lautaro.leonhardt01@gmail.com>
2023-07-04 20:31:09 -0300

    test: items.spec.js

---
+++ src/__tests__/items.spec.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import Items from '../Items'
+
+describe('Items', () => {
+  it('should render all items', () => {
+    const items = [
+      { id: 1, name: 'item 1', completed: false },
+      { id: 2, name: 'item 2', completed: true },
+    ]
+    const mockRemoveItem = jest.fn()
+    const mockEditItem = jest.fn()
+    render(<Items items={items} removeItem={mockRemoveItem} editItem={mockEditItem} />)
+
+    expect(screen.getByText(/item 1/i)).toBeInTheDocument()
+    expect(screen.getByText(/item 2/i)).toBeInTheDocument()
+  })
+
+  it('should not render a list if there are no items', () => {
+    const items = []
+    const mockRemoveItem = jest.fn()
+    const mockEditItem = jest.fn()
+    render(<Items items={items} removeItem={mockRemoveItem} editItem={mockEditItem} />)
+    const list = screen.queryAllByRole('checkbox')
+    expect(list).toHaveLength(0)
+  })
+})
